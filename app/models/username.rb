2e4cb8d2f1d96a4f108a732e7b32a219d4e3789f
Julio <juliodevasquez@gmail.com>
2018-01-02 19:54:12 -0600

    working API call

--- app/models/username.rb
+++ app/models/username.rb
@@ -8,11 +8,13 @@ validates :username,  presence: true
 
 
 	def api_call(username)
-	   
-	    url = "http://ws.audioscrobbler.com/2.0/?method=user.getlovedtracks&user=#{username}&api_key=#{LASTFM_KEY_ID}&format=json"
+	    api_key = ENV['LASTFM_API']
+	    url = "http://ws.audioscrobbler.com/2.0/?method=user.getlovedtracks&user=#{username}&api_key=#{api_key}&limit=10&format=json"
 		open(url) do |f|
  	  		json_string = f.read
  	  		parsed_json = JSON.parse(json_string)
+			 parsed_json["lovedtracks"]["track"].each {|track|
+			 p track['name'] + '- ' +  track['artist']['name'] }
  		end
 	end
 end
