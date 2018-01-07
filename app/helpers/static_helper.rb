97299ff056d305c63bc746c4510378dfb550ec50
Julio <juliodevasquez@gmail.com>
2018-01-07 15:33:27 -0600

    working on regex for video id

--- app/helpers/static_helper.rb
+++ app/helpers/static_helper.rb
@@ -1,7 +1,24 @@
 module StaticHelper
 require "uri"
+require 'nokogiri'
+require 'open-uri'
+
 	def video_id
-		uri = URI.parse('https://www.youtube.com/results?search_query=DNA.-+Kendrick+Lamar')
-	p	uri.inspect
+		doc = Nokogiri::HTML(open('https://www.youtube.com/results?search_query=DNA.-+Kendrick+Lamar'))
+		 links = []
+		 video_link = doc.css('a#video-title.yt-simple-endpoint style-scope ytd-video-renderer' , 'a').each do |fire|
+		 links << fire['href']
+		end
+		 regex = /youtu(?:.*\/v\/|.*v\=|\.be\/)([A-Za-z0-9_\-]{11})/
+		 links.each do |link|
+		 p link
+		 match = link.match(regex)
+			 if match 
+			 	return match[1]
+			 else
+			 	'nothing'
+			end
+		end
 	end
 end
+
