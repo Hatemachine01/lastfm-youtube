c86c992349fd8b06cff3a94ff2db954bf196873d
Julio <juliodevasquez@gmail.com>
2018-03-17 13:52:32 -0400

    embedded video player for a better user experience

--- app/assets/javascripts/video_player.js
+++ app/assets/javascripts/video_player.js
@@ -34,8 +34,8 @@
       var player;
       function onYouTubePlayer(id) {
         player = new YT.Player('player', {
-          height: '360',
-          width: '540',
+          height: '200',
+          width: '300',
           videoId: id,
           events: {
             'onReady': onPlayerReady,
