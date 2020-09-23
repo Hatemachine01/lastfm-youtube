a55ab440f63ab7729e0fa5bc69ecc59161fbd41d
Julio Vasquez <juliodevasquez@gmail.com>
2020-09-23 00:35:15 -0700

    Integrated youtube scraper api + minor fixes

--- app/helpers/static_helper.rb
+++ app/helpers/static_helper.rb
@@ -3,25 +3,14 @@ require "uri"
 require 'nokogiri'
 require 'open-uri'
 require "i18n"
+require 'json'
 
 	def video_id(song)
 		#gets video id by scraping youtubes search result page
-		 song = sanitizer(song) #sanitizes song title to prevent encoding issues
-		 url = 'https://www.youtube.com/results?search_query='+"#{song}"
-		 doc = Nokogiri::HTML(open(url))
-		 links = []
-		 video_link = doc.css('a#video-title' '.yt-simple-endpoint style-scope ytd-video-renderer' , 'a').each do |fire|
-		 links << fire['href']
-		end
-		 regex = /(?<=[?&]v=)[^&$]+/
-		 links.each do |link|
-		 match = link.match(regex)
-			 if match 
-			 	return  match[0]
-			 else
-			 	'nothing'
-			end
-		end
+		song = sanitizer(song) #sanitizes song title to prevent encoding issues
+		json =  open("http://youtube-scrape.herokuapp.com/api/search?q=""#{song}""&page=1").read
+		data = JSON.parse(json)
+		id = data['results'][1]['video']['id']	 
 	end
 
 	private 
